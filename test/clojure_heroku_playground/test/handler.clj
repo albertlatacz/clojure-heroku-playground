073b71c5009c9ac4b8ab17e74f01fc362f1edede
Albert Latacz <albert.latacz@gmail.com>
2013-04-17 09:09:00 +0100

    first commit

---
+++ test/clojure_heroku_playground/test/handler.clj
@@ -0,0 +1,14 @@
+(ns clojure-heroku-playground.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        clojure-heroku-playground.handler))
+
+;(deftest test-app
+;  (testing "main route"
+;    (let [response (app (request :get "/"))]
+;      (is (= (:status response) 200))
+;      (is (= (:body response) "Hello World"))))
+;
+;  (testing "not-found route"
+;    (let [response (app (request :get "/invalid"))]
+;      (is (= (:status response) 404)))))
