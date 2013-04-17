7bc036f77f7cc0af118062966fa0de44e50f291b
Albert Latacz <albert.latacz@gmail.com>
2013-04-17 11:31:09 +0100

    start on correct port and added main

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clojure-heroku-playground "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :aot  'clojure-heroku-playground.main
+  :main clojure-heroku-playground.main
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.1.6"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler clojure-heroku-playground.handler/app}
---
+++ src/clojure_heroku_playground/main.clj
@@ -0,0 +1,8 @@
+(ns clojure-heroku-playground.main
+  (:use clojure-heroku-playground.handler
+        [ring.adapter.jetty :only [run-jetty]]))
+
+(require '[clojure-heroku-playground.handler :as web])
+
+(defn -main [& m]
+  (run-jetty #'web/app {:port (Integer. (get (System/getenv) "PORT" "8085"))}))
