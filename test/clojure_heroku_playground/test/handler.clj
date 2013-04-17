4182ab8460ebb3e05c3ac58088ab31ef6e9be05c
Albert Latacz <albert.latacz@gmail.com>
2013-04-17 12:59:14 +0100

    enabled tests

--- test/clojure_heroku_playground/test/handler.clj
+++ test/clojure_heroku_playground/test/handler.clj
@@ -3,12 +3,12 @@
         ring.mock.request  
         clojure-heroku-playground.handler))
 
-;(deftest test-app
-;  (testing "main route"
-;    (let [response (app (request :get "/"))]
-;      (is (= (:status response) 200))
-;      (is (= (:body response) "Hello World"))))
-;
-;  (testing "not-found route"
-;    (let [response (app (request :get "/invalid"))]
-;      (is (= (:status response) 404)))))
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
